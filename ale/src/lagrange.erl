8eea4c3cca6253d6efeee69201dbc545d989c31a
Vladimir Kirillov <proger@wilab.org.ua>
2016-03-27 00:25:36 +0200

    the magic of math

--- ale/src/lagrange.erl
+++ ale/src/lagrange.erl
@@ -2,12 +2,13 @@
 -behaviour(gen_server).
 
 -export([start_link/0, go/1, left/1, notify/1, listen/0, listen_loop/0]).
+-export([pidcontrol/3]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
--record(state, {encoder_base, encoder_target, encoder_state, encoder_rate}).
+-record(state, {encoder_base, encoder_target, encoder_state, motion, pid}).
 
 %% api
 
@@ -37,24 +38,30 @@ init([]) ->
     {ok, #state{encoder_base=enil(),
                 encoder_state=enil(),
                 encoder_target=enil(),
-                encoder_rate=rnil()}}.
+                pid={0,0,0}}}.
 
 handle_call(hello, _From, State) ->
     lagrange:notify(hello),
     {reply, hello, State};
 handle_call({go, Holes}, _From, State) ->
-    wheels:motor({forward, 100}, {forward, 100}),
+    Motion = [{forward, 100}, {forward, 100}],
+    apply(wheels, motor, Motion),
     {reply, ok, State#state{encoder_base=State#state.encoder_state,
                             encoder_target=bump_target(State#state.encoder_state,
-                                                       Holes)}};
+                                                       Holes),
+                            pid={0,0,0},
+                            motion=Motion}};
 handle_call({left, Degrees}, _From, State) ->
-    wheels:motor({forward, 100}, {forward, 0}),
+    Motion = [{forward, 100}, {forward, 0}],
+    apply(wheels, motor, Motion),
     Base = State#state.encoder_state,
     {encoder, S1, S2} = Base,
     {reply, ok, State#state{encoder_base=Base,
                             encoder_target={encoder,
                                             S1,
-                                            S2 + trunc(Degrees / (360 / 64))}}};
+                                            S2 + trunc(Degrees / (360 / 64))},
+                            pid={0,0,0},
+                            motion=Motion}};
 handle_call(_Request, _From, State) -> {reply, meh, State}.
 
 %% info
@@ -63,19 +70,30 @@ handle_info(hello, State) ->
     io:format("lagrange got hello! ~n", []),
     {noreply, State};
 
-handle_info(E = {encoder, S1, S2}, State = #state{encoder_target={encoder, T1, T2},
-                                                  encoder_rate={rate, R1, R2}}) ->
-    NRate = {rate, S1 - R1, S2 - R2},
+handle_info(E = {encoder, _, _}, State = #state{motion=false}) ->
+    {noreply, State#state{encoder_state=E}};
+
+handle_info(E = {encoder, S1, S2}, State = #state{encoder_base={encoder, B1, B2},
+                                                  encoder_target={encoder, T1, T2},
+                                                  motion=M}) ->
+
+    NewE = {encoder, S1 - B1, S2 - B2},
+
     case S1 >= T1 andalso S2 >= T2 of
         true ->
             wheels:stop(),
-            lagrange:notify({reached, E}),
+            lagrange:notify({reached, {encoder, T1, T2}, E}),
             {noreply, State#state{encoder_base=E,
-                                  encoder_rate=NRate,
-                                  encoder_state=E}};
+                                  encoder_state=E,
+                                  pid={0,0,0},
+                                  motion=false}};
         false ->
-            {noreply, State#state{encoder_rate=NRate,
-                                  encoder_state=E}}
+            {PID, NewMotion} = pidcontrol(State#state.pid, NewE, M),
+            lagrange:notify({correction, PID, NewMotion, NewE}),
+            apply(wheels, motor, NewMotion),
+
+            {noreply, State#state{encoder_state=E,
+                                  pid=PID}}
     end;
 
 handle_info({reached, _}, State) -> {noreply, State};
@@ -92,10 +110,23 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
 enil() -> {encoder, 0, 0}.
 rnil() -> {rate, 0, 0}.
-bump_target({encoder, E1, E2}, Value) -> {encoder, E1 + Value, E2 + Value}.
+bump_target({encoder, E1, E2}, Value) ->
+    {encoder, E1 + Value, E2 + Value}.
 
 listen_loop() ->
     receive
         Event -> io:format("lagrange_events: ~p~n", [Event])
     end,
     ?MODULE:listen_loop().
+
+% lagrange:listen(), lagrange:go(100).
+
+pidcontrol({_, I, D}, {encoder, Left, Right}, [{Direction, LS}, {Direction, RS}]) ->
+    Prop = Left - Right,
+    Int = I + Prop,
+    Diff = Prop - D,
+    Correction = trunc(Prop + 0.5 * Int + Diff),
+    {{Prop, Int, Diff}, [{Direction, LS - Correction}, {Direction, RS + Correction}]}.
+
+millis() ->
+    erlang:monotonic_time(milli_seconds).
